6a7befeda9c5814c782342327b153db22b00bcec
fsenore <fsenore@b0e3558a-1426-0410-83de-bf875247a1ac>
2024-05-13 15:54:15 +0000

    Changed version to 1.5.0.2.
    git-svn-id: svn://svn.code.sf.net/p/vvvapp/code/trunk@473 b0e3558a-1426-0410-83de-bf875247a1ac

--- src/utils.cpp
+++ src/utils.cpp
@@ -32,7 +32,7 @@
 #include <iostream>
 
 wxString CUtils::applicationName = wxT("VVV");
-wxString CUtils::applicationVersion = wxT("1.5.0.1");
+wxString CUtils::applicationVersion = wxT("1.5.0.2");
 int CUtils::expectedDatabaseVersion = 24;
 int CUtils::firstUnicodeDatabaseVersion = 14;
 wxString CUtils::strucUpdateDbName = wxT("vvv-struct-update.fdb");
